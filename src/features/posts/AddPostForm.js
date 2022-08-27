658ce6118078b177ecf64744e1a9e9ac41278835
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-27 15:00:40 +0300

    Add post author component

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { postAdded } from './postsSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
   const dispatch = useDispatch()
+
+  const users = useSelector((state) => state.users)
 
   const onTitleChanged = (e) => {
     setTitle(e.target.value)
@@ -15,13 +18,25 @@ export const AddPostForm = () => {
     setContent(e.target.value)
   }
 
+  const onAuthorChanged = (e) => {
+    setUserId(e.target.value)
+  }
+
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content))
+      dispatch(postAdded(title, content, userId))
       setTitle('')
       setContent('')
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
     <section>
@@ -35,6 +50,11 @@ export const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {userOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
@@ -42,7 +62,7 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked}>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
