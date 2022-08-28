b03076c391cf268a46f1b8e85e5d4e2217d9cd62
Virag Kormoczy <vkormoczy@gmail.com>
2022-08-28 15:08:56 +0300

    Add extra reducers

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -49,6 +49,20 @@ const postsSlice = createSlice({
       }
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        state.posts = state.posts.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
+  },
 })
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
